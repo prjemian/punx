cd5054d829dde2eeae47afd1512724a102702450
prjemian <prjemian@gmail.com>
2016-05-23 13:45:25 -0500

    licensing comment

--- src/punx/cache.py
+++ src/punx/cache.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2016, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
 '''
 maintain the local cache of NeXus NXDL and XML Schema files

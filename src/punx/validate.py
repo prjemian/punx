367c0b85559227980acdf099749f7b0ec825dd27
prjemian <prjemian@gmail.com>
2016-05-27 16:07:52 -0500

    API refactored to simplify documentation of attributes & links

--- src/punx/validate.py
+++ src/punx/validate.py
@@ -71,11 +71,11 @@ class Data_File_Validator(object):
         '''start the validation process'''
         self.examine_group(self.h5, 'NXroot')
 
-    def new_finding(self, h5_object, severity, comment):
+    def new_finding(self, h5_address, severity, comment):
         '''
         accumulate a list of findings
         '''
-        f = finding.Finding(h5_object, severity, comment)
+        f = finding.Finding(str(h5_address), severity, comment)
         self.findings.append(f)
 
     def get_hdf5_attribute(self, obj, attribute, default=None):
@@ -97,16 +97,17 @@ class Data_File_Validator(object):
         nx_class = self.get_hdf5_attribute(group, 'NX_class')
         if nx_class is None:
             if nxdl_classname == 'NXroot':
-                self.new_finding(group, finding.TODO, 'hdf5 file')
+                self.new_finding(group.name, finding.TODO, 'hdf5 file')
             else:
-                self.new_finding(group, finding.NOTE, 'hdf5 group has no `NX_class` attribute')
+                self.new_finding(group.name, finding.NOTE, 'hdf5 group has no `NX_class` attribute')
         else:
-            self.new_finding(group, finding.OK, 'NX_class=' + nx_class)
+            self.new_finding(group.name, finding.OK, 'NX_class=' + nx_class)
         
         # HDF5 group attributes
         for item in sorted(group.attrs.keys()):
+            # TODO: check item name against regular expression
             if item not in ('NX_class',):
-                self.new_finding(group, finding.TODO, '@' + item + ': --TBA--')
+                self.new_finding(group.name + '@' + item, finding.TODO, '--TBA--')
 
         # TODO: special case for NXentry
         # TODO: special case for NXsubentry
@@ -118,17 +119,19 @@ class Data_File_Validator(object):
         
         # HDF5 group children
         for item in sorted(group):
+            # TODO: check item name against regular expression
             obj = group.get(item)
             if h5structure.isHdf5Group(obj):
                 obj_nx_class = self.get_hdf5_attribute(obj, 'NX_class')
                 if obj_nx_class in defined_nxdl_list:
                     self.examine_group(obj, obj_nx_class)
                 else:
-                    self.new_finding(obj, finding.NOTE, 'not defined in ' + nxdl_classname)
+                    # TODO: is group name flexible?
+                    self.new_finding(obj.name, finding.NOTE, 'not defined in ' + nxdl_classname)
             elif h5structure.isHdf5Dataset(obj):
                 self.examine_dataset(obj, group)
             else:
-                self.new_finding(obj, finding.TODO, '--TBA--')
+                self.new_finding(obj.name, finding.TODO, '--TBA--')
 
     
     def examine_dataset(self, dataset, group):
@@ -142,13 +145,15 @@ class Data_File_Validator(object):
         nxdl_class_obj = self.nxdl_dict[nx_class]
         ds_name = dataset.name.split('/')[-1]
         if ds_name not in nxdl_class_obj.fields:
-            self.new_finding(dataset, finding.NOTE, 'unspecified field')
+            # TODO: is dataset name flexible?
+            self.new_finding(dataset.name, finding.NOTE, 'unspecified field')
         else:
-            self.new_finding(dataset, finding.TODO, '--TBA--')
+            self.new_finding(dataset.name, finding.TODO, '--TBA--')
 
         # HDF5 dataset attributes
         for item in sorted(dataset.attrs.keys()):
-            self.new_finding(dataset, finding.TODO, '@' + item + ': --TBA--')
+            # TODO: check item name against regular expression
+            self.new_finding(dataset.name + '@' + item, finding.TODO, '--TBA--')
 
 
 def parse_command_line_arguments():

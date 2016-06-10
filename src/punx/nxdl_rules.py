2d32fffa12a0feee423af319f4297ef8649db18d
jemian <prjemian@gmail.com>
2016-06-10 14:23:09 -0500

    typo

--- src/punx/nxdl_rules.py
+++ src/punx/nxdl_rules.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from spyderlib.widgets.externalshell import namespacebrowser
 
 #-----------------------------------------------------------------------------
 # :author:    Pete R. Jemian

aefe9e9e5d80a09cd9b5858580b0eb28f166f94d
Pete R Jemian <prjemian@gmail.com>
2016-06-29 10:38:45 -0500

    refs #42

--- src/punx/issue_42.py
+++ src/punx/issue_42.py
@@ -4,26 +4,35 @@
 issue #42: test some code to replace the use of urllib2
 '''
 
-import urllib2
-import json
+# import urllib2
+# import json
 #import requests
 import requests.packages.urllib3
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import StringIO
+import zipfile
 
 
-url_commits = 'https://api.github.com/repos/prjemian/punx/commits'
-text = urllib2.urlopen(url_commits).read()
-buf = json.loads(text)
-
-url = u'https://github.com/prjemian/punx/archive/master.zip'
-u = urllib2.urlopen(url)
-content = u.read()
-
-print len(buf), len(content)
-# prints: 30 4050903
+url_commits = 'https://api.github.com/repos/nexusformat/definitions/commits'
+url_zip = u'https://github.com/nexusformat/definitions/archive/master.zip'
 
 
+# requests.exceptions.ConnectionError
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 r = requests.get(url_commits, verify=False)
 print r.status_code
 # prints: 200
+
+latest = r.json()[0]
+
+sha = latest['sha']
+iso8601 = latest['commit']['committer']['date']
+print latest
+
+
+content = requests.get(url_zip).content
+buf = StringIO.StringIO(content)
+zip_content = zipfile.ZipFile(buf)
+
+print len(zip_content.namelist())
+print zip_content.namelist()

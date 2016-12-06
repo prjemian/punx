1f2262c4eda778b9bf3716d16cd5bfc53023fa18
prjemian <prjemian@gmail.com>
2016-12-06 17:00:12 -0600

    refs #51 : test cases setup

--- tests/validate_test.py
+++ tests/validate_test.py
@@ -3,6 +3,7 @@
 test the punx validation process
 '''
 
+import h5py
 import os
 import sys
 import unittest
@@ -41,14 +42,82 @@ class Validate_compression(common.ValidHdf5File):
     testfile = 'compression.h5'
     expected_output = common.read_filelines('validate_compression.txt')
     NeXus = False
-     
+ 
+ 
+class Validate_NXdata_is_now_optional_51(unittest.TestCase):
+
+    def setUp(self):
+        self.testfile = common.getTestFileName()
+
+    def tearDown(self):
+        common.cleanup()
+
+    def test_simple_NXdata(self):
+        import punx.validate, punx.finding, punx.logs
+
+        hdf5root = h5py.File(self.testfile, "w")
+
+        entry = hdf5root.create_group("entry")
+        entry.attrs['NX_class'] = 'NXentry'
+        data = entry.create_group("data")
+        data.attrs['NX_class'] = 'NXdata'
+        entry.attrs["signal"] = "data"
+        data.create_dataset("data", data="a string of characters")
+
+        hdf5root.close()
+        
+        punx.logs.ignore_logging()
+        validator = punx.validate.Data_File_Validator(self.testfile)
+        validator.validate()
+        self.report = []
+        
+        report = validator.report_findings(punx.finding.VALID_STATUS_LIST)
+        #print('\n' + report + '\n')
+        msg = 'NeXus default plot: /entry/data/data'
+        self.assertFalse(report.find('no default plot: not a NeXus file') >= 0, msg)
+        
+        report = validator.report_findings_summary().splitlines()[6]
+        #print('\n' + report + '\n')
+        msg = 'no ERRORs found'
+        self.assertEqual(int(report.split()[1]), 0, msg)
+
+    def test_simple_no_NXdata(self):
+        import punx.validate, punx.finding, punx.logs
+
+        hdf5root = h5py.File(self.testfile, "w")
+
+        entry = hdf5root.create_group("entry")
+        entry.attrs['NX_class'] = 'NXentry'
+
+        hdf5root.close()
+        
+        punx.logs.ignore_logging()
+        validator = punx.validate.Data_File_Validator(self.testfile)
+        validator.validate()
+        self.report = []
+        
+        report = validator.report_findings(punx.finding.VALID_STATUS_LIST)
+        #print('\n' + report + '\n')
+        msg = 'no NeXus default plot, no NXdata group, valid NeXus as of NIAC2016'
+        self.assertFalse(report.find('no default plot: not a NeXus file') >= 0, msg)
+        
+        report = validator.report_findings_summary().splitlines()[6]
+        #print('\n' + report + '\n')
+        msg = 'no ERRORs found'
+        self.assertEqual(int(report.split()[1]), 0, msg)
+
 
 def suite(*args, **kw):
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(Validate_writer_1_3))
-    test_suite.addTest(unittest.makeSuite(Validate_writer_2_1))
-    test_suite.addTest(unittest.makeSuite(Validate_33id_spec_22_2D))
-    test_suite.addTest(unittest.makeSuite(Validate_compression))
+    test_list = [
+        Validate_writer_1_3, 
+        Validate_writer_2_1, 
+        Validate_33id_spec_22_2D, 
+        Validate_compression, 
+        Validate_NXdata_is_now_optional_51,
+        ]
+    for test_case in test_list:
+        test_suite.addTest(unittest.makeSuite(test_case))
     return test_suite
 
 

ab9a7b90cdb93b3ed0891d47428ccb56cd214b8b
jemian <prjemian@gmail.com>
2016-12-08 15:59:06 -0600

    refs #57 prep the unit tests

--- tests/validate_test.py
+++ tests/validate_test.py
@@ -7,6 +7,7 @@ import h5py
 import os
 import sys
 import unittest
+from test.test_xmlrpc import ADDR
 
 _path = os.path.join(os.path.dirname(__file__), '..', )
 if _path not in sys.path:
@@ -162,6 +163,80 @@ class Validate_example_mapping_issue_53(common.CustomHdf5File):
         self.assertEqual(validator.report_findings(punx.finding.ERROR), "None")
         validator.h5.close()
 
+
+class Validate_issue_57(unittest.TestCase):
+    
+    def test_order_of_appearance__observation_1(self):
+        import punx.validate, punx.finding, punx.logs
+        import numpy
+
+        # construct a test file and check it
+        fname = common.getTestFileName()
+        hdf5root = h5py.File(fname, 'w')
+        entry = hdf5root.create_group("entry")
+        entry.attrs['NX_class'] = 'NXentry'
+        data = entry.create_group("data")
+        data.attrs['NX_class'] = 'NXdata'
+        data.attrs["signal"] = "data"
+        ds = data.create_dataset("data", data=[[1,2,3], [3,1,2]])
+        ds.attrs['target'] = ds.name
+        entry['ds_link'] = ds
+        # TODO: may need more items to match error reported in issue #57
+        hdf5root.close()
+
+        punx.logs.ignore_logging()
+        validator = punx.validate.Data_File_Validator(fname)
+        validator.validate()
+        # print('\n'.join(sorted(validator.addresses)))
+        self.assertEqual(len(validator.addresses), 9)
+
+        addresses_to_check_in_this_order = '''
+        /
+        /entry
+        /entry@NX_class
+        /entry/ds_link
+        /entry/ds_link@target
+        /entry/data
+        /entry/data/data
+        /entry/data@NX_class
+        /entry/data@signal
+        '''.split()
+        addresses = list(validator.addresses.keys())
+        for order, addr in enumerate(addresses_to_check_in_this_order):
+            msg = 'HDF5 address not found: ' + addr
+            self.assertTrue(addr in validator.addresses, msg)
+            msg = 'expect ' + addr
+            msg += ' on row ' + str(order)
+            msg += ', found on row ' + str(addresses.index(addr))
+            self.assertEqual(addr, addresses[order], msg)
+    
+    def test_inconsistent_findings__observation_2(self):
+        import punx.validate, punx.finding, punx.logs
+        fname = common.punx_data_file_name('example_mapping.nxs')
+        punx.logs.ignore_logging()
+        validator = punx.validate.Data_File_Validator(fname)
+        validator.validate()
+        # print('\n'.join(sorted(validator.addresses)))
+        # self.assertEqual(len(validator.addresses), 114)
+        addresses_to_check = '''
+        /entry1/instrument/fluo/transformations@NX_class
+        /entry1/instrument/fluo/transformations
+        /entry1/instrument/fluo/data@units
+        /entry1/instrument/fluo/energy@units
+        /entry1/instrument/fluo/transformations/detector_arm
+        /entry1/instrument/fluo/transformations@NX_class
+        /entry1/instrument/trans/transformations/monitor_arm
+        /entry1/sample/t_stage_set@units
+        /entry1/sample/x_stage_set@units
+        /entry1/sample/y_stage_set@units
+        /entry1/user/username
+        /entry1/user@NX_class
+        /entry1/instrument/fluo/transformations@target
+        '''.split()
+        for addr in sorted(addresses_to_check):
+            msg = 'HDF5 address not found: ' + addr
+            self.assertTrue(addr in validator.addresses, msg)
+
  
 def suite(*args, **kw):
     test_suite = unittest.TestSuite()
@@ -171,8 +246,9 @@ def suite(*args, **kw):
         Validate_33id_spec_22_2D, 
         Validate_compression, 
         Validate_NXdata_is_now_optional_51,
-#         Validate_example_mapping, # FIXME: fails both tests
+        Validate_example_mapping, # FIXME: fails both tests
         Validate_example_mapping_issue_53,
+        Validate_issue_57,
         ]
     for test_case in test_list:
         test_suite.addTest(unittest.makeSuite(test_case))

554ba99f1cf8500edcf4a00f3e071ab202608e86
jemian <prjemian@gmail.com>
2016-12-19 22:33:18 -0600

    drop unit test to py 3.4

--- src/punx/__init__.py
+++ src/punx/__init__.py
@@ -71,6 +71,8 @@ __classifiers__ = [
      'Programming Language :: Python',
      'Programming Language :: Python :: 2',
      'Programming Language :: Python :: 2.7',
+     'Programming Language :: Python :: 3',
+     'Programming Language :: Python :: 3.4',
      'Topic :: Scientific/Engineering',
      'Topic :: Scientific/Engineering :: Astronomy',
      'Topic :: Scientific/Engineering :: Bio-Informatics',

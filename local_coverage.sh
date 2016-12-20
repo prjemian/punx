2ac2665d5c6b78be50df9b6a946523bdd8bb2400
jemian <prjemian@gmail.com>
2016-12-20 17:38:27 -0600

    developer support

--- local_coverage.sh
+++ local_coverage.sh
@@ -2,11 +2,6 @@
 
 # file: local_coverage.sh
 
-coverage run tests/common_test.py
-coverage run -a tests/_version_test.py
-coverage run -a tests/cache_test.py
-coverage run -a tests/h5structure_test.py
-coverage run -a tests/logs_test.py
-coverage run -a tests/validate_test.py
+coverage run tests
 coverage html
 coverage report

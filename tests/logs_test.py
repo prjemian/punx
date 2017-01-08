6313cad0be8b155e7a96ca9fc2deddd61e1d4d05
Pete Jemian <prjemian@gmail.com>
2017-01-07 21:50:38 -0600

    refs #67: capture stdout from logger

--- tests/logs_test.py
+++ tests/logs_test.py
@@ -6,11 +6,29 @@ test punx tests/common module (supports unit testing)
 import os
 import sys
 import unittest
+from six import StringIO
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
 import punx
 import punx.logs
+
+
+class Capture_stdout(list):
+    '''
+    capture all printed output (to stdout) into list
+    
+    # http://stackoverflow.com/questions/16571150/how-to-capture-stdout-output-from-a-python-function-call
+    '''
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
 
 class TestLogs(unittest.TestCase):
     
@@ -27,7 +45,9 @@ class TestLogs(unittest.TestCase):
         os.remove(logger.log_file)
 
     def test_console_only(self):
-        logger = punx.logs.Logger(level=punx.CONSOLE_ONLY)
+        with Capture_stdout() as printed_lines:
+            logger = punx.logs.Logger(level=punx.CONSOLE_ONLY)
+        # could test output in: printed_lines
         self.assertEqual(punx.CONSOLE_ONLY, logger.level)
         self.assertEqual(None, logger.log_file)
      

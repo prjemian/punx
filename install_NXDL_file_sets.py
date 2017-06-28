e87f8e1105cfc336991f43fe96d0a70414ca1d29
jemian <prjemian@gmail.com>
2017-06-28 18:03:49 -0500

    add command line arguments to installer

--- install_NXDL_file_sets.py
+++ install_NXDL_file_sets.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+
+"""
+manage (report and/or install) NeXus NXDL file sets 
+"""
+
 import os, sys
 from collections import OrderedDict
 import logging
@@ -11,31 +16,73 @@ import punx.github_handler, punx.cache_manager
 logging.basicConfig(level=logging.DEBUG)   # report everything logged from DEBUG
 
 
-if __name__ == '__main__':
+def install(cm, grr, ref, use_user_cache = True):
+    """
+    Install or update the named NXDL file reference
+    """
+    force = ref == "master"    # always update from the master branch
+
+    msg = " install_NXDL_file_set(ref=%s, force=%s, user_cache=%s)"
+    msg = msg % (ref, str(force), str(use_user_cache))
+    logging.info(msg)
+
+    m = cm.install_NXDL_file_set(
+        grr, 
+        user_cache=use_user_cache, 
+        ref=ref,
+        force = force)
+    if isinstance(m, list):
+        print(str(m[-1]))
+
+
+def main():
+    command_args = parse_command_Line()
     cm = punx.cache_manager.CacheManager()
-    grr = punx.github_handler.GitHub_Repository_Reference()
-    grr.connect_repo()
-    cm.find_all_file_sets()
+    print(cm.table_of_caches())
+
+    if command_args.try_to_install_or_update:
+        grr = punx.github_handler.GitHub_Repository_Reference()
+        grr.connect_repo()
+        cm.find_all_file_sets()
+        
+        # releases = OrderedDict()
+        # releases[punx.github_handler.DEFAULT_RELEASE_NAME] = False  # source cache
+        # releases[punx.github_handler.DEFAULT_BRANCH_NAME] = True
+        # releases[punx.github_handler.DEFAULT_TAG_NAME] = True
+        # releases[punx.github_handler.DEFAULT_COMMIT_NAME] = True
+        # 
+        # for ref, use_user_cache in releases.items():
+        #     install(ref, use_user_cache)
+        
+        install(cm, grr, command_args.ref)
     
-    releases = OrderedDict()
-    releases[punx.github_handler.DEFAULT_RELEASE_NAME] = False  # source cache
-    releases[punx.github_handler.DEFAULT_BRANCH_NAME] = True
-    releases[punx.github_handler.DEFAULT_TAG_NAME] = True
-    releases[punx.github_handler.DEFAULT_COMMIT_NAME] = True
-    
-    for ref, use_user_cache in releases.items():
-        force = ref == "master"    # always update from the master branch
+        print(cm.table_of_caches())
 
-        msg = " install_NXDL_file_set(ref=%s, force=%s, user_cache=%s)"
-        msg = msg % (ref, str(force), str(use_user_cache))
-        logging.info(msg)
 
-        m = cm.install_NXDL_file_set(
-            grr, 
-            user_cache=use_user_cache, 
-            ref=ref,
-            force = force)
-        if isinstance(m, list):
-            print(str(m[-1]))
+def parse_command_Line():
+    import argparse
+    doc = __doc__.strip().splitlines()[0]
 
-    print(punx.cache_manager.table_of_caches())
+    parser = argparse.ArgumentParser(description=doc)
+    help_text = "name of reference NeXus NXDL file set"
+    help_text += " (GitHub tag, hash, version, or 'master')"
+    help_text += " -- default: master"
+    parser.add_argument(
+        '-r', '--ref',
+        default="master",
+        nargs='?',
+        help=help_text)
+
+    parser.add_argument("-i",
+        action='store_false', 
+        default=True,
+        dest='try_to_install_or_update',
+        help='Do not install (or update)')
+
+    results = parser.parse_args()
+
+    return results
+
+
+if __name__ == '__main__':
+    main()

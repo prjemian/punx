9a47b935a3288a37755ebcd4762ac5c5f96995a6
Pete R. Jemian <prjemian@gmail.com>
2017-06-28 23:04:43 -0500

    command-line parameters

--- install_NXDL_file_sets.py
+++ install_NXDL_file_sets.py
@@ -12,15 +12,15 @@ import logging
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 import punx.github_handler, punx.cache_manager
 
+DEFAULT_LOGGING_LEVEL = logging.DEBUG   # report everything logged from DEBUG
+logging.basicConfig(level=DEFAULT_LOGGING_LEVEL)
 
-logging.basicConfig(level=logging.DEBUG)   # report everything logged from DEBUG
 
-
-def install(cm, grr, ref, use_user_cache = True):
+def install(cm, grr, ref, use_user_cache = True, force = False):
     """
     Install or update the named NXDL file reference
     """
-    force = ref == "master"    # always update from the master branch
+    force = force or ref == "master"    # always update from the master branch
 
     msg = " install_NXDL_file_set(ref=%s, force=%s, user_cache=%s)"
     msg = msg % (ref, str(force), str(use_user_cache))
@@ -37,6 +37,10 @@ def install(cm, grr, ref, use_user_cache = True):
 
 def main():
     command_args = parse_command_Line()
+
+    if 0 <= command_args.logging_level < 50:
+    	logging.getLogger().setLevel(command_args.logging_level)
+
     cm = punx.cache_manager.CacheManager()
     print(cm.table_of_caches())
 
@@ -45,17 +49,9 @@ def main():
         grr.connect_repo()
         cm.find_all_file_sets()
         
-        # releases = OrderedDict()
-        # releases[punx.github_handler.DEFAULT_RELEASE_NAME] = False  # source cache
-        # releases[punx.github_handler.DEFAULT_BRANCH_NAME] = True
-        # releases[punx.github_handler.DEFAULT_TAG_NAME] = True
-        # releases[punx.github_handler.DEFAULT_COMMIT_NAME] = True
-        # 
-        # for ref, use_user_cache in releases.items():
-        #     install(ref, use_user_cache)
+        for ref in command_args.file_set_list:
+            install(cm, grr, ref, force=command_args.force)
         
-        install(cm, grr, command_args.ref)
-    
         print(cm.table_of_caches())
 
 
@@ -64,22 +60,41 @@ def parse_command_Line():
     doc = __doc__.strip().splitlines()[0]
 
     parser = argparse.ArgumentParser(description=doc)
-    help_text = "name of reference NeXus NXDL file set"
+    help_text = "name(s) of reference NeXus NXDL file set"
     help_text += " (GitHub tag, hash, version, or 'master')"
-    help_text += " -- default: master"
+    help_text += " -- default master"
     parser.add_argument(
-        '-r', '--ref',
-        default="master",
-        nargs='?',
+        '-r', '--file_set_list',
+        default=["master", ],
+        nargs='*',
         help=help_text)
 
-    parser.add_argument("-i",
+    parser.add_argument("-i", "--install",
         action='store_false', 
         default=True,
         dest='try_to_install_or_update',
-        help='Do not install (or update)')
+        help='Do not install (or update) -- default True')
+
+    parser.add_argument("-f", "--force",
+        action='store_true', 
+        default=False,
+        dest='force',
+        help='Force install (or update) - default False')
+
+    help_text = "logging level: 0 .. 50 -- default " 
+    help_text += str(DEFAULT_LOGGING_LEVEL)
+    parser.add_argument(
+        '-l', '--logging_level',
+        type=int,
+        default=DEFAULT_LOGGING_LEVEL,
+        help=help_text)
 
     results = parser.parse_args()
+
+    logging.debug("file sets: " + " ".join(results.file_set_list))
+    logging.debug("install: " + str(results.try_to_install_or_update))
+    logging.debug("force: " + str(results.force))
+    logging.debug("logging: " + str(results.logging_level))
 
     return results
 

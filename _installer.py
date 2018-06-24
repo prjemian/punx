8fb50e8506700d8c4d68b587dcf389a5bb74e3b3
Pete Jemian <prjemian@gmail.com>
2018-06-24 13:00:15 -0500

    make installation of NeXus defs easier

--- _installer.py
+++ _installer.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
 
-"manage (report and/or install) NeXus NXDL file sets "
+"""
+manage (report and/or install) NeXus NXDL file sets
+
+Installs into user configuration directory
+"""
 
 import logging
 import sys
 import install_NXDL_file_sets
 
+DEFAULT_REF = "v2018.5"
 
 DEFAULT_LOGGING_LEVEL = logging.DEBUG   # report everything logged from DEBUG
 logging.basicConfig(level=DEFAULT_LOGGING_LEVEL)
 
-sys.argv += "-r v2018.5".split()
+if len(sys.argv) == 1:
+    sys.argv.append("-r")
+    sys.argv.append(DEFAULT_REF)
 install_NXDL_file_sets.main()

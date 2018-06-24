f13a6f4afc67eaa76a9ed88c3afc448aaa81686c
Pete Jemian <prjemian@gmail.com>
2018-06-24 12:41:30 -0500

    to install v2018.5 definitions

--- _installer.py
+++ _installer.py
@@ -11,5 +11,5 @@ import install_NXDL_file_sets
 DEFAULT_LOGGING_LEVEL = logging.DEBUG   # report everything logged from DEBUG
 logging.basicConfig(level=DEFAULT_LOGGING_LEVEL)
 
-sys.argv += "-r v3.3".split()
+sys.argv += "-r v2018.5".split()
 install_NXDL_file_sets.main()

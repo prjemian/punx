2c4c16d9f0e525cb9e91f218c733f147bdb74c44
Pete Jemian <prjemian@gmail.com>
2018-07-02 09:29:57 -0500

    #105 utility to process all data files

---
+++ check_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# file: check_data.sh
+# test that punx program can process all the files in the data directory
+
+DATA_DIR=src/punx/data
+
+for f in `ls -1 ${DATA_DIR}`; do
+   echo src/punx/data/$f
+   ./_starter_.py tree ${DATA_DIR}/$f
+   ./_starter_.py validate ${DATA_DIR}/$f
+done

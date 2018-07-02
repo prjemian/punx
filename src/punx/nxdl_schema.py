a7fba0152ddc5f3507db1c944da5f12a72fd54f7
Pete Jemian <prjemian@gmail.com>
2018-07-02 00:15:02 -0500

    copyright

--- src/punx/nxdl_schema.py
+++ src/punx/nxdl_schema.py
@@ -4,7 +4,7 @@
 #-----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
 # :email:     prjemian@gmail.com
-# :copyright: (c) 2017, Pete R. Jemian
+# :copyright: (c) 2017-2018, Pete R. Jemian
 #
 # Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
 #

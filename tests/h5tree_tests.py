35582ca40ce245b7fa3bf55748b6efe7e815abf5
Pete Jemian <prjemian@gmail.com>
2020-06-29 16:32:47 -0500

    TST #123

---
+++ tests/h5tree_tests.py
@@ -0,0 +1,68 @@
+
+'''
+test punx h5tree module
+'''
+
+import os
+import sys
+import h5py
+import h5py.h5g
+import numpy
+import tempfile
+import unittest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+import punx, punx.h5tree, punx.utils
+
+
+class No_Exception(Exception): pass
+
+
+class Test_Battery(unittest.TestCase):
+
+    def setUp(self):
+        # create the test file
+        tfile = tempfile.NamedTemporaryFile(suffix='.hdf5', delete=False)
+        tfile.close()
+        self.hfile = tfile.name
+     
+    def tearDown(self):
+        if self.hfile is not None and os.path.exists(self.hfile):
+            os.remove(self.hfile)
+        self.hfile = None
+
+    def test_h5tree(self):
+        self.assertIsNotNone(self.hfile)
+        self.assertTrue(os.path.exists(self.hfile))
+        self.assertFalse(punx.utils.isHdf5FileObject(self.hfile))
+        str_list = [
+            "Q=1",
+            "Q=0.1",
+            "Q=0.01",
+            "Q=0.001",
+            "Q=0.0001",
+            "Q=0.00001",
+        ]
+        with h5py.File(self.hfile) as f:
+            self.assertFalse(punx.utils.isHdf5FileObject(self.hfile))
+            self.assertTrue(f)
+            f.create_dataset('str_list', data=[s.encode("utf8") for s in str_list])
+
+        mc = punx.h5tree.Hdf5TreeView(self.hfile)
+        txt = mc.report()
+        self.assertEqual(len(txt), len(str_list)+1)
+
+
+def suite(*args, **kw):
+    test_suite = unittest.TestSuite()
+    test_list = [
+        Test_Battery,
+        ]
+    for test_case in test_list:
+        test_suite.addTest(unittest.makeSuite(test_case))
+    return test_suite
+
+
+if __name__ == '__main__':
+    runner=unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())

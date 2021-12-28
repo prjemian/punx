b7b090f07bb780499d1831888b83725aa6560de0
Pete R Jemian <prjemian@gmail.com>
2021-12-28 17:09:47 -0600

    TST #183

--- punx/validations/tests/test_hdf5_group_items_in_base_class.py
+++ punx/validations/tests/test_hdf5_group_items_in_base_class.py
@@ -1,4 +1,5 @@
 import h5py
+import pytest
 
 from ..hdf5_group_items_in_base_class import verify_group_attributes
 from ... import nxdl_manager
@@ -6,8 +7,19 @@ from ... import validate
 from ...tests._core import hfile
 
 
-def test_verify_group_attributes(hfile):
+@pytest.mark.parametrize(
+    "h5addr, nx_class, extra_findings",
+    [
+        ["/", "NXroot", 5],
+        ["/entry", "NXentry", 4],
+        ["/entry/data", "NXdata", 4],
+        ["/link", "NXdata", 4],
+        ["/e2", "NXentry", 4],
+    ]
+)
+def test_verify_group_attributes(h5addr, nx_class, extra_findings, hfile):
     with h5py.File(hfile, "w") as root:
+        root.attrs["NX_class"] = "NXroot"
         root.attrs["default"] = "entry"
 
         nxentry = root.create_group("entry")
@@ -24,6 +36,9 @@ def test_verify_group_attributes(hfile):
         root["link"] = nxdata
         nxdata.attrs["target"] = nxdata.name
 
+        root["e2"] = nxentry
+        nxentry.attrs["target"] = nxentry.name
+
     validator = validate.Data_File_Validator()
     assert validator is not None
 
@@ -31,13 +46,13 @@ def test_verify_group_attributes(hfile):
     v0 = len(validator.validations)
     assert v0 > 0
 
-    v_item = validator.addresses.get("/entry/data")
+    v_item = validator.addresses.get(h5addr)
     assert v_item is not None
     assert isinstance(v_item, validate.ValidationItem)
 
-    base_class = validator.manager.classes.get("NXdata")
+    base_class = validator.manager.classes.get(nx_class)
     assert base_class is not None
     assert isinstance(base_class, nxdl_manager.NXDL__definition)
 
     verify_group_attributes(validator, v_item, base_class)
-    assert len(validator.validations) == v0 + 4
+    assert len(validator.validations) - v0 == extra_findings

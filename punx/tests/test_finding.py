25adbebb442fd65fcfed663578b45c32b88ac0dc
Pete R Jemian <prjemian@gmail.com>
2022-01-01 13:26:05 -0600

    TST #183 test the hash

--- punx/tests/test_finding.py
+++ punx/tests/test_finding.py
@@ -33,7 +33,7 @@ def test_exception(h5_address, test_name, status, comment, xceptn):
         ["A", "this", finding.OK, "looks good"],
     ]
 )
-def test_str(addr, test_name, status, comment):
+def test_Finding_str(addr, test_name, status, comment):
     expect = (
         "finding.Finding("
         f"{addr}"
@@ -53,3 +53,24 @@ def test_standard():
     key_list = list(sorted(map(str, finding.TF_RESULT.keys())))
     k2 = list(sorted(map(str, (False, True))))
     assert key_list == k2
+
+
+@pytest.mark.parametrize(
+    "addr, test_name, status, comment",
+    [
+        [None, None, finding.ERROR, None],
+        ["h5_address", "test_name", finding.NOTE, "comment"],
+        ["A", "this", finding.OK, "looks good"],
+    ]
+)
+def test_Finding_make_md5(addr, test_name, status, comment):
+    f = finding.Finding(addr, test_name, status, comment)
+    if addr is None:
+        with pytest.raises(TypeError):
+            f.make_md5()
+    else:
+        md5 = f.make_md5()
+        assert isinstance(md5, str)
+
+        # can be duplicated from same inputs (is NOT random)
+        assert md5 == f.make_md5()

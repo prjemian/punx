0f07827ae308abcb6aaa47b355d6e7d86426dc83
Pete R Jemian <prjemian@gmail.com>
2023-08-29 09:12:39 -0500

    CI #224 try testing with py3.11 & py3.12

--- punx/__init__.py
+++ punx/__init__.py
@@ -86,6 +86,8 @@ __classifiers__ = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Astronomy",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
